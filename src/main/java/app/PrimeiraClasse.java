321b5ca8ad90931ba8aab3d8d8309a23d0b6df01
RaihanyGm <raihany.gmendes@gmail.com>
2025-08-22 01:40:21 +0000

    Aula do Marcos

--- src/main/java/app/PrimeiraClasse.java
+++ src/main/java/app/PrimeiraClasse.java
@@ -2,6 +2,12 @@ package app;
 
 public class PrimeiraClasse {
     public static void main(String[] args) {
+        Aluno al = new Aluno();
+        al.setNome("raihany");
+        al.setIdade(20);
+
+        System.out.println(al.get.nome());
+        System.out.println(al.get.idade());
         System.out.println("Olá Gradle!!!");
     }
 }
